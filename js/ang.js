40c2cdbb8fb8a4164c98dfc5c502ae7380d306e8
OneOfCats <krableshtraus@gmail.com>
2016-01-14 14:12:20 +0200

    commit changes

--- js/ang.js
+++ js/ang.js
@@ -11,9 +11,11 @@ app.controller('appController', ['$scope', function($scope){
   $scope.updateUser = function updateUser(){
     if($scope.userData.userId === undefined) return;
     var requireUserObject = VK.Api.call('users.getSubscriptions', {user_id: parseInt($scope.userData.userId), extended: 1}, function(r){
-      if(!r.response) return;
-      $scope.userData.userSubscriptions = r.response.slice();
-      $scope.searchByThisPublic = $scope.userData.userSubscriptions[0].name;
+      $scope.$apply(function(){
+        if(!r.response) return;
+        $scope.userData.userSubscriptions = r.response.slice();
+        $scope.searchByThisPublic = $scope.userData.userSubscriptions[0].name;
+      });
     });
     return requireUserObject;
   };
@@ -33,12 +35,14 @@ app.controller('appController', ['$scope', function($scope){
 
     function callUserSearch(r){
       if(!r.response || r.response.users.length == 0) return;
-      for(var i = 0; i < r.response.users.length; i++){
-        $scope.usersFound.push(r.response.users[i]);
-      }
-      console.log($scope.usersFound.length);
-      offsetLength += 1000;
-      VK.Api.call('groups.getMembers', {group_id: publicId, count: 1000, offset: offsetLength, fields: 'sex, photo_200'}, callUserSearch);
+      $scope.$apply(function(){
+        for(var i = 0; i < r.response.users.length; i++){
+          $scope.usersFound.push(r.response.users[i]);
+        }
+        console.log($scope.usersFound.length);
+        offsetLength += 1000;
+        VK.Api.call('groups.getMembers', {group_id: publicId, count: 1000, offset: offsetLength, fields: 'sex, photo_200'}, callUserSearch);
+      });
     }
   };
 
