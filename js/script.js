390517abc66dc717f3df6ec2a1be88d3810d8feb
OneOfCats <krableshtraus@gmail.com>
2016-01-14 13:32:11 +0200

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 ﻿document.addEventListener('click', clickListen);
+document.addEventListener('keypress', clickListen);
 
 function clickListen(event){
   var targ = event.target;
@@ -16,5 +17,13 @@ function clickListen(event){
   if(targ.hasAttribute('data-toggle')){
     targ.classList.toggle(targ.getAttribute('data-toggle'));
   }
-  return
+  return;
+}
+
+function keypressListen(event){
+  if(event.keyCode != "T".charCodeAt(0)) return;
+  var elems = document.querySelectorAll('[data-toggle-key]');
+  for(var i = 0; i < elems.length; i++){
+    elems[i].classList.toggle(elems[i].getAttribute('data-toggle-key'));
+  }
 }
